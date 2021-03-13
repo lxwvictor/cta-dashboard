3d31a8f07fc5088d6fd9c10f1838a7c510d6c98e
Li Xiaowei <lxwvictor@gmail.com>
2021-03-13 10:28:16 +0800

    finished step 4

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -33,16 +33,16 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("connect_stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("TransformedStation", partitions=1, value_type=TransformedStation)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+   'table',
+   default=TransformedStation,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
 #
@@ -52,6 +52,26 @@ app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memor
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
 #
 #
+
+@app.agent(topic)
+async def tranform_station(stations):
+    async for station in stations:
+        if station.red:
+            line = 'red'
+        elif station.blue:
+            line = 'blue'
+        elif station.green:
+            line = 'green'
+        else:
+            logger.warn('No line color')
+            line = None
+
+        table[station.station_id] = TransformedStation(
+            station.station_id,
+            station.station_name,
+            station.order,
+            line
+        )
 
 
 if __name__ == "__main__":
--- producers/connector.py
+++ producers/connector.py
@@ -51,7 +51,7 @@ def configure_connector():
                # TODO
                "incrementing.column.name": "stop_id",
                # TODO
-               "topic.prefix": "chicago_",
+               "topic.prefix": "connect_",
                # TODO
                "poll.interval.ms": "5000",
            }
